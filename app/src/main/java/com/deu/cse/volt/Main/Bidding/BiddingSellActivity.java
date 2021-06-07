777e6712207bba443098542104d209d7d559068c
unknown <mhhh2082@gmail.com>
2021-06-07 23:41:52 +0900

    bidding activity

---
+++ app/src/main/java/com/deu/cse/volt/Main/Bidding/BiddingSellActivity.java
@@ -0,0 +1,14 @@
+package com.deu.cse.volt.Main.Bidding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BiddingSellActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bidding_sell);
+    }
+}
